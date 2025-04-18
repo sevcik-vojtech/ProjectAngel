f93504853dc7d010c7023ced04539fdbfd736e5e
Krystof Hubacek <hubackry@fit.cvut.cz>
2025-04-18 18:36:20 +0200

    fix_character_body_2d

---
+++ projectangel/scripts/character_body_2d.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+var speed = 300
+
+func get_input():
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	velocity = input_dir * speed
+	if Input.is_action_pressed("left_click"):
+		print("left_click")
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+	
