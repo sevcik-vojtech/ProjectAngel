0352f1fff865659a3f57c017eb3c445a13625ce6
petrmartinek <martinek.petr@proton.me>
2025-04-20 00:27:03 +0200

    start of cutscene

---
+++ projectangel/scenes/cutscenes/play_dialogue.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+var resource;
+
+func play_dialogue(dialogue : String, start : String):
+	resource = load("res://resources/dialogues/" + dialogue + ".dialogue");
+	DialogueManager.show_dialogue_balloon(resource, start)
+	
