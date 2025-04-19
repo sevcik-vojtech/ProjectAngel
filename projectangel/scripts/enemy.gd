bab4bc9a67c73f0deda86bb61870aa2dec1e7887
sevcik-vojtech <sevcikvojtech2@gmail.com>
2025-04-19 12:24:11 +0200

    Add functional enemy attacks

--- projectangel/scripts/enemy.gd
+++ projectangel/scripts/enemy.gd
@@ -1,11 +1,10 @@
 extends CharacterBody2D
 
-@export var attack_range: float = 1.0
-@export var attack_cooldown: float = 2.0
-
 var player: CharacterBody2D = null
 var can_attack: bool = true
 var health := 100
+var area_is_in_hitbox: bool = false
+var area_in_hitbox: Area2D = null
 
 func take_damage(amount: int):
 	health -= amount
@@ -18,28 +17,32 @@ func die():
 	queue_free()
 
 func _ready():
-	var _player = get_tree().get_nodes_in_group("player").front()
+	player = get_tree().get_nodes_in_group("player").front()
 	$EnemyAnimation.play("default")
 	add_to_group("enemy")
 
 func _process(_delta):
-	if $EnemyAnimation.frame == 2:
-		$EnemyAnimation/Hitbox.monitoring = true;
-	else:
-		$EnemyAnimation/Hitbox.monitoring = false;
-	
-	if player:
-		var distance_to_player = global_position.distance_to(player.global_position)
-		
-		if distance_to_player <= attack_range and can_attack:
-			attack_player()
-
-func attack_player():
-	$EnemyAnimation.play("attack_animation")
-	can_attack = false
-	await get_tree().create_timer(attack_cooldown).timeout
-	can_attack = true
+	if area_is_in_hitbox:
+		if area_in_hitbox.get_parent().is_in_group("player") and can_attack:
+			$EnemyAnimation.play("attack_animation")
+			
+		if area_in_hitbox.get_parent().is_in_group("player") and can_attack and $EnemyAnimation.frame == 2:
+			area_in_hitbox.get_parent().take_damage()
+			print("Damaging the player")
+			can_attack = false
+			$AttackCooldown.start()
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
-	if area.is_in_group("player"):
-		area.take_damage(1)
+	area_is_in_hitbox = true
+	area_in_hitbox = area
+
+func _on_hitbox_area_exited(area: Area2D) -> void:
+	area_is_in_hitbox = false
+	$EnemyAnimation.play("default")
+
+
+func _on_attack_cooldown_timeout() -> void:
+	can_attack = true
+
+func _on_enemy_animation_finished() -> void:
+	$EnemyAnimation.play("default")
