c26c485a3f6eb73d0cb6853a240b387ad2bf31fa
Krystof Hubacek <hubackry@fit.cvut.cz>
2025-04-19 11:00:23 +0200

    enemy can take damage

--- projectangel/scripts/projectile_1.gd
+++ projectangel/scripts/projectile_1.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var speed = 750
+@export var damage = 10
 const RANGE = 5000
 var travelled_distance = 0
 
@@ -14,4 +15,6 @@ func _physics_process(delta):
 
 
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.is_in_group("enemy"):
+		body.take_damage(damage)
+		queue_free()
